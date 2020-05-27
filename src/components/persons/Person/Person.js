86d648d6d764b9d272eda647d757b5ec17ad20e5
nada tarek  galal <nadagalal1995@gmail.com>
2020-05-28 01:24:26 +0200

    [110] Ref

--- src/components/persons/Person/Person.js
+++ src/components/persons/Person/Person.js
@@ -1,43 +1,40 @@
-import React from 'react';
+import React,{Component, createRef} from 'react';
 import styled from 'styled-components'
 import Aux from '../../../hoc/Auxiliary'
 import withClass from '../../../hoc/WithClass';
 import classes from './Person.css';
 import propTypes from 'prop-types'
 //import Radium  from 'radium';
-const person = (props) => {
-    const StyleDiv = styled.div`
-    width: 60%;
-    margin:16px auto;
-    border:1px solid #eee;
-    box-shadow:0 2px 3px #ccc;
-    padding:16px;
-    text-align: center;
-    '@media (min-width : 500PX)' : {
-        width : '450px;'
-    
-    
-    
-    `;
-    // const style = {
-    //     '@media (min-width : 500PX)' : {
-    //         width : '450px;'
-    //     }
-    // }
+class Person extends Component {
+    constructor(props){
+        super(props);
+        this.inputElement = React.createRef();
+    }
+    componentDidMount(){
+       // this.inputElement.focus();
+       this.inputElement.current.focus();
+    }
+    render(){
     return(
         //<div className='Person' style={style}>
         <Aux>
-        <p key="i1" onClick = {props.click} > I am {props.name} , I have {props.age} years </p>
-        <p key="i2"> {props.children} </p>
-        <input key="i3" type="text" onChange = {props.change} value={props.name} /> 
+        <p key="i1" onClick = {this.props.click} > I am {this.props.name} , I have {this.props.age} years </p>
+        <p key="i2" > {this.props.children} </p>
+        <input key="i3" 
+        //ref= { (inputEl) => {this.inputElement = inputEl}}
+        ref ={this.inputElement}
+        type="text" 
+        onChange = {this.props.change} 
+        value={this.props.name} /> 
         </Aux>
        
         );
+    }
 }
-person.propTypes = {
+Person.propTypes = {
     click : propTypes.func ,
     name : propTypes.string,
-    age :propTypes.String,
+    age :propTypes.string,
     change : propTypes.func
 }
-export default withClass(person,classes.Person);
+export default withClass(Person,classes.Person);
